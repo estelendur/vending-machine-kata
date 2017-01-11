37387a39dc38df7165a4db8bfc70d9e04e82ad8a
Esty Thomas <kethomas2342@gmail.com>
2017-01-10 20:20:53 -0500

    Factored out initialization of vending machine.

--- run_tests.py
+++ run_tests.py
@@ -3,27 +3,26 @@ from vendingmachine import *
 
 class TestVendingMachine(unittest.TestCase):
 
+    def setUp(self):
+        self.vend = VendingMachine()
+
     def test_display_initially_shows_insert_coins(self):
-        vend = VendingMachine()
-        self.assertEqual("INSERT COIN", vend.display())
+        self.assertEqual("INSERT COIN", self.vend.display())
 
     def test_machine_accepts_dime(self):
-        vend = VendingMachine()
-        vend.insert_coin("dime")
-        self.assertEqual("0.10", vend.display())
+        self.vend.insert_coin("dime")
+        self.assertEqual("0.10", self.vend.display())
 
     def test_machine_shows_total_inserted_money(self):
-        vend = VendingMachine()
-        vend.insert_coin("dime")
-        vend.insert_coin("nickel")
-        vend.insert_coin("quarter")
-        self.assertEqual("0.40", vend.display())
+        self.vend.insert_coin("dime")
+        self.vend.insert_coin("nickel")
+        self.vend.insert_coin("quarter")
+        self.assertEqual("0.40", self.vend.display())
 
     def test_machine_rejects_penny(self):
-          vend = VendingMachine()
-          vend.insert_coin("penny")
-          self.assertEqual("INSERT COIN", vend.display())
-          self.assertEqual(["penny"], vend.returned_coins)
+          self.vend.insert_coin("penny")
+          self.assertEqual("INSERT COIN", self.vend.display())
+          self.assertEqual(["penny"], self.vend.returned_coins)
 
 if __name__ == '__main__':
     unittest.main()
